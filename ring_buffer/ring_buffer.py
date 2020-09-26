6eb7210dd57c632e9bae9434529631a7d1092926
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-09-25 21:56:17 -0400

    finsihed ring_buffer. tests passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.items = []
+        self.capacity = capacity
+        self.oldest = 0
+
 
     def append(self, item):
-        pass
+        if len(self.items) < self.capacity:
+            self.items.append(item)
+        else:
+            self.items[self.oldest] = item
+            self.oldest += 1
+        if self.oldest == self.capacity:
+            self.oldest = 0
 
     def get(self):
-        pass
+        return self.items
